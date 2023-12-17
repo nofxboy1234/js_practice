53845b4d4fd5364767300e7f871609ae14b9684c
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-17 15:58:18 +0200

    Updcate createUser Factory Function with private variable -- reputation

--- main.js
+++ main.js
@@ -356,6 +356,17 @@
 
 function createUser(name) {
   const discordName = '@' + name;
-  return { name, discordName };
-}
 
+  let reputation = 0;
+  const getReputation = () => reputation;
+  const giveReputation = () => reputation++;
+
+  return { name, discordName, getReputation, giveReputation };
+}
+const josh = createUser('josh');
+josh.giveReputation();
+josh.giveReputation();
+console.log({
+  discordName: josh.discordName,
+  reputation: josh.getReputation(),
+});
