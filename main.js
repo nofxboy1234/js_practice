74058f041fbc92463c9a9a95ab10b1e73f354c44
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-20 08:17:00 +0200

    Test ClassWithPrivate from MDN

--- main.js
+++ main.js
@@ -1358,30 +1358,67 @@
 // }
 // console.log(Article.publisher);
 
-class Animal {
-  static planet = 'Earth';
+// class Animal {
+//   static planet = 'Earth';
 
-  constructor(name, speed) {
-    this.speed = speed;
-    this.name = name;
+//   constructor(name, speed) {
+//     this.speed = speed;
+//     this.name = name;
+//   }
+
+//   run(speed = 0) {
+//     this.speed += speed;
+//     console.log(`${this.name} runs with speed ${this.speed}.`);
+//   }
+
+//   static compare(animalA, animalB) {
+//     return animalA.speed - animalB.speed;
+//   }
+// }
+// // Inherit from Animal
+// class Rabbit extends Animal {
+//   hide() {
+//     console.log(`${this.name} hides!`);
+//   }
+// }
+// let rabbits = [new Rabbit('White Rabbit', 10), new Rabbit('Black Rabbit', 5)];
+// rabbits.sort(Rabbit.compare);
+// rabbits[0].run(); // Black Rabbit runs with speed 5.
+// console.log(Rabbit.planet); // Earth
+
+// const calculatorMixin = (Base) =>
+//   class extends Base {
+//     calc() {
+//       console.log('calculate stuff');
+//     }
+//   };
+// const randomizerMixin = (Base) =>
+//   class extends Base {
+//     randomize() {
+//       console.log('randomize stuff');
+//     }
+//   };
+// class Foo {}
+// class Bar extends calculatorMixin(randomizerMixin(Foo)) {}
+// console.log('end');
+
+class ClassWithPrivate {
+  #privateField;
+  #privateFieldWithInitializer = 42;
+
+  #privateMethod() {
+    console.log('This is a private method');
   }
 
-  run(speed = 0) {
-    this.speed += speed;
-    console.log(`${this.name} runs with speed ${this.speed}.`);
+  static #privateStaticField;
+  static #privateStaticFieldWithInitializer = 42;
+
+  static #privateStaticMethod() {
+    console.log('This is a private static method');
   }
 
-  static compare(animalA, animalB) {
-    return animalA.speed - animalB.speed;
+  hello() {
+    console.log(this.#privateFieldWithInitializer);
   }
 }
-// Inherit from Animal
-class Rabbit extends Animal {
-  hide() {
-    console.log(`${this.name} hides!`);
-  }
-}
-let rabbits = [new Rabbit('White Rabbit', 10), new Rabbit('Black Rabbit', 5)];
-rabbits.sort(Rabbit.compare);
-rabbits[0].run(); // Black Rabbit runs with speed 5.
-console.log(Rabbit.planet); // Earth
+const instanceOfClassWithPrivate = new ClassWithPrivate();
